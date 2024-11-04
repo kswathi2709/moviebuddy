4bbaa826ce85a5010213136a202a58df9d98fce8
Swathi <swathikan2709@gmail.com>
2024-11-04 21:04:16 +0530

    final changes

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -18,7 +18,7 @@ export const Search = ({apiPath}) => {
   return (
     <main>
          <section className="py-7">
-        <p className="text-3xl text-gray-700 dark:text-white">{ movies.length === 0 ? `No result found for '${queryTerm}'` : `Result for '${queryTerm}'` }</p>
+        <p className="text-3xl text-gray-700 dark:text-white">{ movies.length === 0 ? `No result found for '${queryTerm}'` : `Results for '${queryTerm}'` }</p>
       </section>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">       
